dfd188a7da008ae1a2fc6027b67dcd34fc06db29
Juixe7 <bms2023007@iiitm.ac.in>
2025-07-03 11:19:30 +0530

    Added Routing for Homepage,login and signup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import {Routes,Route} from "react-router"
+import Login from "./pages/Login"
+import Homepage from "./pages/Homepage"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+
 function App(){
   return(
     <>
-    Hello Coder Army
+    <Routes>
+      <Route path="/" element={<Homepage></Homepage>}></Route>
+      <Route path="/login" element={<Login></Login>}></Route>
+      <Route path="/signup" element={<Signup></Signup>}></Route>
+    </Routes>
     </>
   )
 }
