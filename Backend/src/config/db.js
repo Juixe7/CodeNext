338ed2f869b8542378e57952758d8d043184cb46
Juixe7 <bms2023007@iiitm.ac.in>
2025-07-05 19:14:50 +0530

    cleared

--- Backend/src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require('dotenv').config();
-async function main(){
-    mongoose.connect(process.env.DB_Connect_String);
-    console.log("Data base is connected!")
-}
-
-module.exports = main;
--- Backend/src/routes/submit.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userMiddleware = require("../middleware/userMiddleware");
-const submissionRouter = express.Router();
-const { submitCode,runCode } = require("../controllers/userSubmission");
-
-submissionRouter.post("/submit/:id",userMiddleware,submitCode);
-submissionRouter.post("/run/:id",userMiddleware,runCode);
-
-module.exports = {submissionRouter};
