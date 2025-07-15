daf5e6be0afa697b735d0aff1d58fd1e1b3cfab0
Juixe7 <bms2023007@iiitm.ac.in>
2025-07-15 16:55:00 +0530

    Update redis.js

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: process.env.REDIS_HOST || 'redis-17648.c301.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 17648,
+        host: process.env.REDIS_HOST || 'redis-16903.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: process.env.REDIS_PORT ? Number(process.env.REDIS_PORT) : 16903,
         reconnectStrategy: (retries) => {
             const delay = Math.min(1000 * Math.pow(2, retries), 10000);
             return delay;
